3067e2482ad28075458fc954fe14bddebd1a8e9a
Rex <xingjialiang@xunlei.com>
2024-04-16 17:31:39 +0800

    [fix] - 修复短文本切换长文本不会动的问题

--- Sources/MarqueeText/MarqueeText.swift
+++ Sources/MarqueeText/MarqueeText.swift
@@ -1,6 +1,9 @@
 import SwiftUI
+import Combine
 
-public struct MarqueeText : View {
+/// 跑马灯效果控件 https://github.com/joekndy/MarqueeText
+
+public struct PPMarqueeText : View {
     public var text: String
     public var font: UIFont
     public var leftFade: CGFloat
@@ -25,62 +28,58 @@ public struct MarqueeText : View {
         
         return ZStack {
             GeometryReader { geo in
-                if stringWidth > geo.size.width { // don't use self.animate as conditional here
-                    Group {
-                        Text(self.text)
-                            .lineLimit(1)
-                            .font(.init(font))
-                            .offset(x: self.animate ? -stringWidth - stringHeight * 2 : 0)
-                            .animation(self.animate ? animation : nullAnimation, value: self.animate)
-                            .onAppear {
-                                DispatchQueue.main.async {
-                                    self.animate = geo.size.width < stringWidth
+                Group {
+                    if stringWidth > geo.size.width { // don't use self.animate as conditional here
+                        Group {
+                            Text(self.text)
+                                .lineLimit(1)
+                                .font(.init(font))
+                                .offset(x: self.animate ? -stringWidth - stringHeight * 2 : 0)
+                                .animation(self.animate ? animation : nullAnimation, value: self.animate)
+                                .onAppear {
+                                    Task { @MainActor in
+                                        self.animate = geo.size.width < stringWidth
+                                    }
                                 }
-                            }
-                            .fixedSize(horizontal: true, vertical: false)
-                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-                        
-                        Text(self.text)
-                            .lineLimit(1)
-                            .font(.init(font))
-                            .offset(x: self.animate ? 0 : stringWidth + stringHeight * 2)
-                            .animation(self.animate ? animation : nullAnimation, value: self.animate)
-                            .onAppear {
-                                DispatchQueue.main.async {
-                                    self.animate = geo.size.width < stringWidth
+                                .fixedSize(horizontal: true, vertical: false)
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                            
+                            Text(self.text)
+                                .lineLimit(1)
+                                .font(.init(font))
+                                .offset(x: self.animate ? 0 : stringWidth + stringHeight * 2)
+                                .animation(self.animate ? animation : nullAnimation, value: self.animate)
+                                .onAppear {
+                                    Task { @MainActor in
+                                        self.animate = geo.size.width < stringWidth
+                                    }
                                 }
-                            }
-                            .fixedSize(horizontal: true, vertical: false)
-                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                                .fixedSize(horizontal: true, vertical: false)
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                        }
+                        .mask(
+                            HStack(spacing:0) {
+                                Rectangle()
+                                    .frame(width:2)
+                                    .opacity(0)
+                                LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0), Color.black]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+                                    .frame(width:leftFade)
+                                LinearGradient(gradient: Gradient(colors: [Color.black, Color.black]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+                                LinearGradient(gradient: Gradient(colors: [Color.black, Color.black.opacity(0)]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+                                    .frame(width:rightFade)
+                                Rectangle()
+                                    .frame(width:2)
+                                    .opacity(0)
+                            })
+                        .frame(width: geo.size.width)
+                    } else {
+                        Text(self.text)
+                            .font(.init(font))
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: alignment)
                     }
-                    .onValueChanged(of: self.text, perform: {text in
-                        self.animate = geo.size.width < stringWidth
-                    })
-                    
-                    .offset(x: leftFade)
-                    .mask(
-                        HStack(spacing:0) {
-                            Rectangle()
-                                .frame(width:2)
-                                .opacity(0)
-                            LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0), Color.black]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
-                                .frame(width:leftFade)
-                            LinearGradient(gradient: Gradient(colors: [Color.black, Color.black]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
-                            LinearGradient(gradient: Gradient(colors: [Color.black, Color.black.opacity(0)]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
-                                .frame(width:rightFade)
-                            Rectangle()
-                                .frame(width:2)
-                                .opacity(0)
-                        })
-                    .frame(width: geo.size.width + leftFade)
-                    .offset(x: leftFade * -1)
-                } else {
-                    Text(self.text)
-                        .font(.init(font))
-                        .onValueChanged(of: self.text, perform: {text in
-                            self.animate = geo.size.width < stringWidth
-                        })
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: alignment)
+                }
+                .onValueChanged(of: self.text) {text in
+                    self.animate = geo.size.width < stringWidth
                 }
             }
         }
@@ -90,7 +89,7 @@ public struct MarqueeText : View {
 
     }
     
-    public init(text: String, font: UIFont, leftFade: CGFloat, rightFade: CGFloat, startDelay: Double, alignment: Alignment? = nil) {
+    public init(text: String, font: UIFont, leftFade: CGFloat = 0, rightFade: CGFloat = 0, startDelay: Double = 0, alignment: Alignment? = nil) {
         self.text = text
         self.font = font
         self.leftFade = leftFade
@@ -100,13 +99,27 @@ public struct MarqueeText : View {
     }
 }
 
-extension MarqueeText {
+extension PPMarqueeText {
     public func makeCompact(_ compact: Bool = true) -> Self {
         var view = self
         view.isCompact = compact
         return view
     }
 }
+
+extension View {
+    /// A backwards compatible wrapper for iOS 14 `onChange`
+    @ViewBuilder func onValueChanged<T: Equatable>(of value: T, perform onChange: @escaping (T) -> Void) -> some View {
+        if #available(iOS 14.0, *) {
+            self.onChange(of: value, perform: onChange)
+        } else {
+            self.onReceive(Just(value)) { (value) in
+                onChange(value)
+            }
+        }
+    }
+}
+
 
 extension String {
     
